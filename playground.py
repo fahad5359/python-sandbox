37ccf5d1003412a18d7e602ead81b5bbe21bf2a6
fahad5359 <fabbgm5359@gmail.com>
2021-10-19 11:52:57 +0300

    practice on python...

---
+++ playground.py
@@ -0,0 +1,77 @@
+# from HW2 import summ
+
+
+# print("hey")
+
+# ###########################functions ###########################
+# to write a function in python u need to write de name():
+
+# def sum():
+#     print("the function is workng")
+
+# meh =sum();
+# print(meh)
+
+# def summm(a,b):
+#     return a+b
+# print(summm(5,5))
+  
+
+############################ dictionarys ############################
+ 
+
+# cars ={
+#     "offroud": "jeep,buggy",
+#     "highwua": "labo,camry",
+#     "trucks": "marcades, gallab"
+#  }
+# # to access an endevgule one ,we need to use as the array way...
+# print(cars["trucks"])
+
+# def frouts(frotname,color,ss):
+
+#     # dictionary: ["apple": "red", "banana": "yellow" , "orange": "orange" ]
+#   ss[frotname]=color
+#   return ss
+
+# print(frouts(ss=[]))
+
+froutscolor ={
+    "apple":"red",
+    "orange":"orange",
+    "banan":"yello",
+}
+
+
+def floop():
+ for x in froutscolor.values():
+    print(x)
+
+print(floop())
+    
+
+# def randm():
+#     froutscolor ={
+#     "apple":"red",
+#     "orange":"orange",
+#     "banan":"yello",
+# }
+    
+
+# def fvall():
+#      print(froutscolor["apple","orange","banan"])
+ 
+
+# print(fvall())
+
+
+  
+
+
+
+
+  
+
+
+    
+
