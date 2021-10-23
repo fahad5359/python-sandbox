078d551e346bb693601594647999d1787f6f39df
fahad5359 <fabbgm5359@gmail.com>
2021-10-24 00:40:13 +0300

    oop

---
+++ claasses.py
@@ -0,0 +1,19 @@
+
+
+class Drinkk:
+    def __init__(self,type,gass_or_not,sizee):
+
+        self.type=type
+
+        self.gass_or_not=gass_or_not
+        
+        self.sizee=sizee
+        
+    def __init__(self) :
+        print("injoi ur drink")
+
+gd = Drinkk("ghazy","yes",440)
+
+
+
+print(gd.type)
