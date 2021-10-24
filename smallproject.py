22bd1e180e0f59440737a196add92ad36168ecff
fahad5359 <fabbgm5359@gmail.com>
2021-10-24 12:57:19 +0300

    finshed main idea

--- smallproject.py
+++ smallproject.py
@@ -1,45 +1,47 @@
+# #############################هنا نبني كلاس الدمية ونعطيها خصائص عامة لكي يتم بنأ ابناء منها انهرتنس.. #######################
 class Targetpractice:
-    def __init__(self,health):
-        self.health=health
+    def __init__(self, health):
+        self.health = health
+
+    def displayhealth(self):
+        return self.health
+
 
 class robot(Targetpractice):
     pass
 
-class Wepon:
-    def __init__(self,name,dmg,recoil,typee,) :
-        self.name=name
-        self.dmg=dmg
-        self.recoil=recoil
-        self.typee=typee
 
+# ############################# Now we start with the wepons type and interaction with the target practice#######################
+
+class Wepon:
+    def __init__(self, name, dmg, recoil, typee,):
+        self.name = name
+        self.dmg = dmg
+        self.recoil = recoil
+        self.typee = typee
 
     def wpninfo(self):
-        print(self.name,"damege =",self.dmg,"wpn recoil =",self.recoil,"It's a",self.typee,"type")
+        print(self.name, "damege =", self.dmg, "wpn recoil =",
+              self.recoil, "It's a", self.typee, "type")
 
     def wpndmg(self):
         return self.dmg
 
 
-
 class heavywpn(Wepon):
-        pass
-
+    pass
 
 
 # p20 = heavywpn("p20",200,"0","heavy")
-p20dmg = heavywpn("p20",200,55,"oo")
+p20dmg = heavywpn("p20", 200, 55, "oo")
 p20dmg.wpndmg()
 # print(p20dmg.wpndmg())
-Roobot = robot(200)
-print(Roobot())
+Roobot = robot(300)
+Roobot.displayhealth()
+# print(Roobot.displayhealth())
 
 
-# if p20dmg.wpndmg() >Roobot():
-#     print("wow the robot is dead")
-# elif p20dmg.wpndmg() < Roobot():
-#     print("this wepon is weak, the robot is not dead")
-
-
-
-
-
+if p20dmg.wpndmg() > Roobot.displayhealth():
+    print("wow the robot is dead")
+elif p20dmg.wpndmg() < Roobot.displayhealth():
+    print("this wepon is weak, the robot is not dead")
